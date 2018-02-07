9b0c13b51bcce116ff8e02f09f95fc1c085abec9
Dori Levy <dori.levy@wework.com>
2018-02-07 15:21:02 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,12 @@
+
+def display_board(board)
+  i = 0 
+  while i < 9
+    print " #{board[i] "
+    if ( i + 1 ) % 3 == 0:
+      puts ""
+      puts "-----------"
+    else 
+      print "|" 
+  end
+end
